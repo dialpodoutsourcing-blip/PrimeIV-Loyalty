b5b6c5eb247b44fce543ee1a5e6305d70e469dcc
dialpodoutsourcing-blip <dialpodoutsourcing@gmail.com>
2026-08-12 13:46:36 -0400

    Redesign customer QR member pass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./admin/analytics.css";
+import "./customer-qr.css";
 import "./signup.css";
 
 export const metadata: Metadata = {
